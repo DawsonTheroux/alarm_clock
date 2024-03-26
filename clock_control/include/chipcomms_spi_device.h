1e882f88238a0965f3572149c0b05576d8b0d653
Dawson Theroux <dawsontheroux@gmail.com>
2024-03-25 23:23:11 -0400

    Implemented SPI DMA for RP2040

--- clock_control/include/chipcomms_spi_device.h
+++ clock_control/include/chipcomms_spi_device.h
@@ -1,10 +1,32 @@
 #ifndef CHIPCOMMS_SPIS_DEVICE_H
 #define CHIPCOMMS_SPIS_DEVICE_H
+#include "hardware/dma.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
 
 #define SPI0_RX_PIN PICO_DEFAULT_SPI_RX_PIN
 #define SPI0_TX_PIN PICO_DEFAULT_SPI_TX_PIN
 #define SPI0_SCK_PIN PICO_DEFAULT_SPI_SCK_PIN
 #define SPI0_CSN_PIN PICO_DEFAULT_SPI_CSN_PIN
+#define SPI_DMA_CHAN 0;
+
+typedef struct spi_irq_data_t{
+  QueueHandle_t spi_rx_queue;
+  uint8_t* spi_temp_rx_buffer;
+  uint16_t current_transfer_len;
+  uint dma_rx_chan;
+  // dma_channel_config* spi_rx_dma_config;
+}spi_irq_data_t;
+
+typedef struct spi_rx_data_t{
+  uint16_t data_length;
+  uint8_t* data;
+} spi_rx_data_t;
+
+
 void spi_read_task(void* args);
+void spi_dma_irq_handler(void);
+void spi_rx_irq_handler(void);
 
 #endif
--- clock_control/src/chipcomms_spi_device.c
+++ clock_control/src/chipcomms_spi_device.c
@@ -7,6 +7,7 @@
 #include "hardware/gpio.h"
 #include "hardware/spi.h"
 #include "hardware/irq.h"
+#include "hardware/dma.h"
 
 // FreeRTOS includes.
 #include "FreeRTOS.h"
@@ -18,75 +19,106 @@
 #include "chipcomms_spi_device.h"
 #include "main.h"
 
-/* NOTE: This could probably be improved
-*        by modifying the ISR to only read what is currently
-*        available and then let the scheduler continue.
-*
-*        For now though, I am expecting small amounts of transfers
-*        so I will leave as is.
-*
-*        This may be a good option in case there is a problem with the line.
-*/
-
-QueueHandle_t spi_rx_queue;
-uint8_t* spi_buffer;
-
-#define MAX_SPI_LEN 512
-
-typedef struct spi_rx_data_t{
-  uint16_t data_length;
-  uint8_t* data;
-  uint8_t command;
-} spi_rx_data_t;
+static spi_irq_data_t spi_irq_data;
 
 void setup_spi()
 {
-    spi_init(spi_default, CHIPCOMMS_SPI_FREQ_HZ);
-    spi_set_slave(spi_default, true);
-    gpio_set_function(SPI0_RX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI0_SCK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI0_TX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI0_CSN_PIN, GPIO_FUNC_SPI);
-    spi_set_format(spi_default,8, SPI_CPOL_0, SPI_CPHA_1, SPI_MSB_FIRST);
-    printf("Set Baud rate: %d - RX pin: %d - TX pin: %d - SCK pin: %d - CS pin: %d\r\n", spi_get_baudrate(spi_default), SPI0_RX_PIN, SPI0_TX_PIN, SPI0_SCK_PIN, SPI0_CSN_PIN);
+  dma_channel_config spi_rx_dma_config;
+  // Setup the SPI channel.
+  spi_init(spi_default, CHIPCOMMS_SPI_FREQ_HZ);
+  spi_set_slave(spi_default, true);
+  gpio_set_function(SPI0_RX_PIN, GPIO_FUNC_SPI);
+  gpio_set_function(SPI0_SCK_PIN, GPIO_FUNC_SPI);
+  gpio_set_function(SPI0_TX_PIN, GPIO_FUNC_SPI);
+  gpio_set_function(SPI0_CSN_PIN, GPIO_FUNC_SPI);
+  spi_set_format(spi_default,8, SPI_CPOL_0, SPI_CPHA_1, SPI_MSB_FIRST);
 
+  // Iniaialize spi_irq_data
+  spi_irq_data.spi_rx_queue = xQueueCreate(16, sizeof(spi_rx_data_t*));
+  spi_irq_data.spi_temp_rx_buffer = NULL;
+  spi_irq_data.current_transfer_len = 0;
+  spi_irq_data.dma_rx_chan = dma_claim_unused_channel(true);
+
+  // Configure SPI RX DMA.
+  spi_rx_dma_config = dma_channel_get_default_config(spi_irq_data.dma_rx_chan);
+  channel_config_set_transfer_data_size(&spi_rx_dma_config, DMA_SIZE_8);
+  channel_config_set_dreq(&spi_rx_dma_config, spi_get_dreq(spi_default, false));
+  channel_config_set_read_increment(&spi_rx_dma_config, false);
+  channel_config_set_write_increment(&spi_rx_dma_config, true);
+  dma_channel_configure(spi_irq_data.dma_rx_chan,
+                        &spi_rx_dma_config,
+                        NULL,                         // set write address later.
+                        &spi_get_hw(spi_default)->dr, // Read address.
+                        0,                            // Set transfer size later.
+                        false);                       // Start transfer NOW.
+
+  // Setup the SPI IRQ.
+  spi_get_hw(spi_default)->imsc |= SPI_SSPIMSC_RXIM_BITS; // Enable interupt on RX buffer !empty
+  irq_set_exclusive_handler(SPI0_IRQ, spi_rx_irq_handler);
+  irq_set_priority(SPI0_IRQ,1);
+  irq_set_enabled(SPI0_IRQ,true);
+
+  // Setup SPI RX DMA IRQ.
+  dma_channel_set_irq0_enabled(spi_irq_data.dma_rx_chan, true);
+  irq_set_exclusive_handler(DMA_IRQ_0, spi_dma_irq_handler);
+  irq_set_priority(DMA_IRQ_0, 0);
+  irq_set_enabled(DMA_IRQ_0, true);
+
+  printf("Set Baud rate: %d - RX pin: %d - TX pin: %d - SCK pin: %d - CS pin: %d\r\n", spi_get_baudrate(spi_default), SPI0_RX_PIN, SPI0_TX_PIN, SPI0_SCK_PIN, SPI0_CSN_PIN);
 }
 
-void spi_irq_handler(void)
+void spi_dma_irq_handler(void)
+/* Handle the DMA DONE IRQ
+ * This should reset the static rpi_irq_data
+ * data instance by resetting the current transfer length
+ * and moving the sip_temp_rx_buffer to NULL
+ */
 {
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  spi_rx_data_t* new_queue_data = (spi_rx_data_t*) malloc(sizeof(spi_rx_data_t));
+  new_queue_data->data_length = spi_irq_data.current_transfer_len;
+  new_queue_data->data = spi_irq_data.spi_temp_rx_buffer;
+  // Reset the values in the spi_irq_data
+  spi_irq_data.spi_temp_rx_buffer = NULL;
+  spi_irq_data.current_transfer_len = 0;
+  // Send the data to the queue, and yield.
+  xQueueSendFromISR(spi_irq_data.spi_rx_queue, &new_queue_data, &xHigherPriorityTaskWoken);
+  // Clear the SPI RX IRQ and DMA IRQ
+  spi_get_hw(spi_default)->icr |= SPI_SSPICR_RTIC_BITS;
+  dma_hw->ints0 = 1u << spi_irq_data.dma_rx_chan;
+  // Reenable the SPI RX interrupt.
+  irq_set_enabled(SPI0_IRQ,true);
+  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+void spi_rx_irq_handler(void)
+/* Handle the RX IRQ for SPI by opening
+ * a DMA channel and starting to listen
+ * for DMA finished.
+ */
+{
+  irq_set_enabled(SPI0_IRQ,false);
+  // Disable the interupt while the transfer is occuring.
+  spi_get_hw(spi_default)->imsc |= SPI_SSPIMSC_RXIM_BITS; // Enable interupt on RX buffer !empty
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
   uint8_t data_parts[2];
   uint8_t* rx_data;
-  spi_rx_data_t* new_queue_data = (spi_rx_data_t*) malloc(sizeof(spi_rx_data_t));
-  // Read the first two bytes of data to get the transmit size.
+  // Read the first two bytes for the data length and malloc.
   spi_read_blocking(spi_default, 0, data_parts, 2);
-  new_queue_data->data_length = data_parts[0] << 8 | data_parts[1];
-  // Allocate the rx data buffer to the data_length.
-  rx_data = (uint8_t*)malloc(new_queue_data->data_length);
-  // Read data_length bytes from the SPI line.
-  spi_read_blocking(spi_default, 0, rx_data, new_queue_data->data_length);
-  new_queue_data->data = rx_data;
-  // Clear the interupt.
-  spi_get_hw(spi_default)->icr |= SPI_SSPICR_RTIC_BITS;
-  // Send the data to the queue, and yield.
-  xQueueSendFromISR(spi_rx_queue, &new_queue_data, &xHigherPriorityTaskWoken);
+  spi_irq_data.current_transfer_len = data_parts[0] << 8 | data_parts[1];
+  spi_irq_data.spi_temp_rx_buffer = (uint8_t*)malloc(spi_irq_data.current_transfer_len);
+  // Set the DMA transfer count.
+  dma_channel_set_trans_count(spi_irq_data.dma_rx_chan, spi_irq_data.current_transfer_len, false);
+  // Set the RX buffer and start the counter.
+  dma_channel_set_write_addr(spi_irq_data.dma_rx_chan, spi_irq_data.spi_temp_rx_buffer, true);
   portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 void spi_read_task(void* args)
 {
   setup_spi();
-  spi_rx_queue = xQueueCreate(16, sizeof(spi_rx_data_t*));
-  spi_get_hw(spi_default)->imsc |= SPI_SSPIMSC_RXIM_BITS;
-  spi_buffer = (uint8_t*)malloc(256);
-
-  irq_set_exclusive_handler(SPI0_IRQ, spi_irq_handler);
-  irq_set_priority(SPI0_IRQ,0);
-  irq_set_enabled(SPI0_IRQ,true);
-  bool failed = false;
   for(;;){
     spi_rx_data_t* spi_rx_data;
-    if(pdTRUE == xQueueReceive(spi_rx_queue, &spi_rx_data, portMAX_DELAY)){
+    if(pdTRUE == xQueueReceive(spi_irq_data.spi_rx_queue, &spi_rx_data, portMAX_DELAY)){
       printf("Received length: %d - Message: ", spi_rx_data->data_length);
       //for(int i=0;i<spi_rx_data.data_length; i++)
       for(int i=0;i<spi_rx_data->data_length; i++)
